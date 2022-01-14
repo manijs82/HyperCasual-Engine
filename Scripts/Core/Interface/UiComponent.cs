1590d189dded607d3cb3b1a444ee79a669594b65
manijs82 <Manijavdani1@gmail.com>
2022-01-14 19:42:31 +0330

    updated ui creator

--- Scripts/Core/Interface/UiComponent.cs
+++ Scripts/Core/Interface/UiComponent.cs
@@ -12,20 +12,20 @@ namespace HyperCasual_Engine
         public UiType type;
         public string objectName;
 
-        [HideInInspector] public Transform tfObject;
+        [HideInInspector] public Transform transform;
         [HideInInspector] public Image image;
         [HideInInspector] public UnityEngine.UI.Button button;
         [HideInInspector] public TextMeshProUGUI textMesh;
         
-        public void CreateComponent(Transform canvas, UiComponent parentComponent, bool isRoot)
+        public void CreateComponent(Transform canvas, UiComponent parentComponent, bool isRoot = false)
         {
             switch (type)
             {
                 case UiType.Button:
-                    CreateButton(isRoot ? canvas : parentComponent.tfObject);
+                    CreateButton(isRoot ? canvas : parentComponent.transform);
                     break;
                 case UiType.Panel:
-                    CreatePanel(isRoot ? canvas : parentComponent.tfObject);
+                    CreatePanel(isRoot ? canvas : parentComponent.transform);
                     break;
                 case UiType.Text:
                     break;
@@ -34,25 +34,21 @@ namespace HyperCasual_Engine
 
         private void CreateButton(Transform parent)
         {
-            tfObject = new GameObject(objectName).transform;
-            tfObject.SetParent(parent);
-
-            image = tfObject.gameObject.AddComponent<Image>();
-            button = tfObject.gameObject.AddComponent<UnityEngine.UI.Button>();
-            var textObj = new GameObject("Text");
-            textObj.transform.SetParent(tfObject);
-            textMesh = textObj.AddComponent<TextMeshProUGUI>();
-            textMesh.text = objectName;
+            DefaultControls.Resources uiResources = new DefaultControls.Resources();
+            var obj = DefaultControls.CreateButton(uiResources);
+            obj.name = objectName;
+            obj.transform.SetParent(parent);
+            transform = obj.transform;
         }
         
         private void CreatePanel(Transform parent)
         {
-            tfObject = new GameObject(objectName).transform;
-            tfObject.SetParent(parent);
-
-            image = tfObject.gameObject.AddComponent<Image>();
-            image.rectTransform.StretchToAllSides();
-            
+            DefaultControls.Resources uiResources = new DefaultControls.Resources();
+            var obj = DefaultControls.CreatePanel(uiResources);
+            obj.name = objectName;
+            obj.transform.SetParent(parent);
+            transform = obj.transform;
+            obj.GetComponent<RectTransform>().StretchToAllSides();
         }
 
         public enum UiType
--- Scripts/Core/Interface/UiCreator.cs
+++ Scripts/Core/Interface/UiCreator.cs
@@ -6,13 +6,13 @@ namespace HyperCasual_Engine
     public class UiCreator : MonoBehaviour
     {
         public UnityObjectTree<UiComponent> componentTree;
-        
+
         public void CreateUi()
         {
             componentTree.rootNode.value.CreateComponent(transform, null, true);
             foreach (var node in componentTree.Collect(componentTree.rootNode))
             {
-                
+                node.value.CreateComponent(transform, node.parent.value);
             }
         }
 
