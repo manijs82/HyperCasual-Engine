8fc7a944fb78302d9a0538f7762d4612382b0002
Mani <Manijavdani1@gmail.com>
2023-01-20 17:07:43 +0100

    2d movement

---
+++ Scripts/Abilities/Movement/Movement2D.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+namespace HyperCasual_Engine.Abilities
+{
+    public class Movement2D : MovementAbility
+    {
+        [SerializeField] private float acceleration = 1;
+        [SerializeField] private float deAcceleration = .5f;
+        
+        private Vector3 moveDirection;
+        private Vector3 velocity;
+        
+        private float Horizontal => Owner.InputManager.horizontal;
+        private float Vertical => Owner.InputManager.vertical;
+        
+        protected override void Move()
+        {
+            UpdateVelocity();
+            transform.position += velocity * Time.deltaTime;
+        }
+
+        private void UpdateVelocity()
+        {
+            var inputDir = new Vector3(Horizontal, Vertical).normalized;
+            moveDirection = inputDir * acceleration;
+            velocity += moveDirection;
+
+            velocity -= velocity * deAcceleration;
+
+            velocity = Vector3.ClampMagnitude(velocity, initialSpeed);
+        }
+    }
+}
